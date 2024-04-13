38ae17d7b424dbd8ed9f851e0f7076794d918c39
mgsulaiman <info@mgsulaiman.com>
2024-04-13 13:45:37 +0300

    init

--- Package.swift
+++ Package.swift
@@ -3,21 +3,21 @@
 
 import PackageDescription
 
+private let analyticsInterface = "AnalyticsInterface"
 let package = Package(
     name: "Analytics",
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Analytics",
             targets: ["Analytics"]),
     ],
+    dependencies: [.package(name: analyticsInterface, path: "../\(analyticsInterface)")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Analytics"),
-        .testTarget(
-            name: "AnalyticsTests",
-            dependencies: ["Analytics"]),
+            name: "Analytics", dependencies: [.product(name: analyticsInterface, package: analyticsInterface)]),
     ]
 )
